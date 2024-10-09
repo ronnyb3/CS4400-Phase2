6d687c22c23330497b077e8355d04f1d12aa5ea8
ronnyb3 <ronny101berry@gmail.com>
2024-10-09 14:50:52 -0400

    Update cs4400_phase2_database_team#94.sql

--- cs4400_phase2_database_team#94.sql
+++ cs4400_phase2_database_team#94.sql
@@ -1,14 +1,14 @@
 CREATE TABLE User (
-    username VARCHAR(50) PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    address VARCHAR(255),
-    birthdate DATE
+    username VARCHAR(40) PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    birthdate DATE NOT NULL
 );
 CREATE TABLE Product (
-    barcode VARCHAR(50) PRIMARY KEY,
+    barcode VARCHAR(40) PRIMARY KEY,
     iname VARCHAR(100) NOT NULL,
-    weight DECIMAL(10, 2)
+    weight INT
 );
 CREATE TABLE Location (
     label VARCHAR(50) PRIMARY KEY,
@@ -18,7 +18,7 @@ CREATE TABLE Location (
 );
 CREATE TABLE Business (
     name VARCHAR(100) PRIMARY KEY,
-    rating DECIMAL(3, 2),
+    rating INT CHECK (BETWEEN 1 and 5),
     spent DECIMAL(15, 2),
     location_label VARCHAR(50),
     FOREIGN KEY (location_label) REFERENCES Location(label)
@@ -47,20 +47,20 @@ CREATE TABLE Van (
     FOREIGN KEY (location_label) REFERENCES Location(label)
 );
 CREATE TABLE Employee (
-    username VARCHAR(50) PRIMARY KEY,
-    taxID VARCHAR(50) NOT NULL,
+    username VARCHAR(40) PRIMARY KEY,
+    taxID VARCHAR(40) NOT NULL,
     hired DATE,
-    salary DECIMAL(15, 2),
+    salary DECIMAL(15, 2) NOT NULL,
     experience INT,
     FOREIGN KEY (username) REFERENCES User(username)
 );
 CREATE TABLE Owner (
-    username VARCHAR(50) PRIMARY KEY,
+    username VARCHAR(40) PRIMARY KEY,
     FOREIGN KEY (username) REFERENCES User(username)
 );
 CREATE TABLE Driver (
-    username VARCHAR(50) PRIMARY KEY,
-    licenseID VARCHAR(50) NOT NULL,
+    username VARCHAR(40) PRIMARY KEY,
+    licenseID VARCHAR(40) NOT NULL,
     license_type VARCHAR(50),
     successful_trips INT,
     FOREIGN KEY (username) REFERENCES User(username)
@@ -79,8 +79,8 @@ CREATE TABLE Fund (
     FOREIGN KEY (business_name) REFERENCES Business(name)
 );
 CREATE TABLE WorkFor (
-    worker_username VARCHAR(50),
-    service_ID INT,
+    worker_username VARCHAR(40),
+    service_ID VARCHAR(40),
     PRIMARY KEY (worker_username, service_ID),
     FOREIGN KEY (worker_username) REFERENCES Worker(username),
     FOREIGN KEY (service_ID) REFERENCES Service(ID)
