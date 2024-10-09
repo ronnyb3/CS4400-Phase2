05ac0658d7b596e9453aba4d377eed847149ba32
ronnyb3 <ronny101berry@gmail.com>
2024-10-09 09:57:59 -0400

    Update cs4400_phase2_database_team#94.sql

--- cs4400_phase2_database_team#94.sql
+++ cs4400_phase2_database_team#94.sql
@@ -5,7 +5,7 @@ CREATE TABLE User (
     address VARCHAR(255),
     birthdate DATE
 );
-#update1
+#update1 #update2
 CREATE TABLE Product (
     barcode VARCHAR(50) PRIMARY KEY,
     iname VARCHAR(100) NOT NULL,
